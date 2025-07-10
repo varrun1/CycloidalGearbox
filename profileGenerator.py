d955873b01db6e0c540715984d568747b8ad9ddb
Varrun Vijay <varrun82@gmail.com>
2025-07-09 21:10:52 -0400

    Added SW Equation Generation

--- profileGenerator.py
+++ profileGenerator.py
@@ -7,6 +7,17 @@ import numpy as np
 # plt.plot(x_points,y_points)
 # plt.show()
 
+#Prints out string equation for SW equation-driven curve
+def swEquation(D,dr,n,e,N):
+    R = D/2
+    Rr = dr/2 
+    n = -n
+    X_eq = "({}*cos(t)) - ({}*cos(t+arctan( sin({}*t)/(({}) - cos({}*t))))) - ({}*cos({}*t)) ".format(R,Rr,n,R/(e*N),n,e,N)
+    Y_eq = "(-{}*sin(t)) + ({}*sin(t+arctan( sin({}*t)/(({}) - cos({}*t))))) + ({}*sin({}*t)) ".format(R,Rr,n,R/(e*N),n,e,N) 
+    print(X_eq)
+    print(Y_eq)
+
+# MAIN CODE
 # Define cycloidal gearbox parameters 
 D = 100 # mm
 N = 30
@@ -16,7 +27,6 @@ e = 0.8 # mm
 
 x_val = []
 y_val = []
-#phi_val = np.radians(np.arange(0,360))
 phi_val = np.linspace(0, 2*np.pi, 10000, endpoint=False)
 
 for phi in phi_val:
@@ -28,6 +38,7 @@ for phi in phi_val:
     x_val.append(x_current)
     y_val.append(y_current)
 
+swEquation(D, d_fp,n,e,N)
 
 plt.plot(x_val, y_val)
 plt.grid(True)
