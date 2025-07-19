8e1044fea2cd20844d7c0ae58ac1d00cab1332e2
Varrun Vijay <varrun82@gmail.com>
2025-07-18 21:33:47 -0400

    Full static plotting implemented

--- profileGenerator.py
+++ profileGenerator.py
@@ -12,27 +12,82 @@ def swEquation(D,dr,n,e,N):
     print(X_eq)
     print(Y_eq)
 
-def plotFullStatic(N, D, d_fp, x_value, y_value, e):
+#Plot static disc profile (at origin)
+def plotStaticProf(x_val, y_val, D): 
+    fig, ax, = plt.subplots()
+    ax.plot(x_val, y_val)
+    shaft = plt.Circle((0, 0), 0.5, color='black')  # Fixed input shaft center
+    ax.add_patch(shaft)
+    base_circle = plt.Circle((0,0),radius = (D/2),fill=False,linestyle='--',edgecolor='red',linewidth=2) # PCD outline plot
+    ax.add_patch(base_circle)
+    ax.set_title('Static Cycloidal Disk Profile')
+    plt.grid(True)
+    plt.axis('equal')
+    plt.show()
+
+#Plot full static cycloidal gearbox profile(s)
+def plotFullStatic(N, D, d_fp, x_value, y_value, e, outputPCD, d_outH, numOutPin, outPinD):
     fig1, ax1, = plt.subplots()
+    disc2Color = 'GoldenRod'
+    # Static 1st disc, offset @ +e
+    x_val_offset = [x+e for x in x_value]
+    y_val_offset = y_value
 
-    x_val_offset = [x-e for x in x_value]
-
+    # Static 2nd disc, 180 deg OoP
+    x_val_180 = [-x for x in x_value]
+    y_val_180 = [-y for y in y_value]
+    x_val_180_shifted = [x - e for x in x_val_180] 
+    
     # Plot fixed ring pins
     thetaRing = (2*np.pi)/N
     for pinNum in range(N):
         pin_cx = (D/2)*np.cos(pinNum*thetaRing)
         pin_cy = (D/2)*np.sin(pinNum*thetaRing)
-        pin = plt.Circle((pin_cx, pin_cy), radius = d_fp/2, color='black')
+        pin = plt.Circle((pin_cx, pin_cy), radius = (d_fp/2), color='black')
         ax1.add_patch(pin)
-    ax1.plot(x_val_offset, y_value)
+
+    # Plot output pin holes on disc #1
+    thetaOutput = (2*np.pi)/numOutPin
+    for pinNum in range(numOutPin):
+        pin_cx = (outputPCD/2)*np.cos(pinNum*thetaOutput) + e
+        pin_cy = (outputPCD/2)*np.sin(pinNum*thetaOutput)
+        pin = plt.Circle((pin_cx, pin_cy), radius = (d_outH/2), fill = False, color = 'blue')
+        ax1.add_patch(pin)
+
+    # Plot output pin holes on disc #2
+    for pinNum in range(numOutPin):
+        pin_cx = (outputPCD/2)*np.cos(pinNum*thetaOutput) - e
+        pin_cy = (outputPCD/2)*np.sin(pinNum*thetaOutput)
+        pin = plt.Circle((pin_cx, pin_cy), radius = (d_outH/2), fill = False, color = disc2Color)
+        ax1.add_patch(pin)    
+
+    # Plot output disc pins
+    for pinNum in range(numOutPin):
+        pin_cx = (outputPCD/2)*np.cos(pinNum*thetaOutput)
+        pin_cy = (outputPCD/2)*np.sin(pinNum*thetaOutput)
+        pin = plt.Circle((pin_cx, pin_cy), radius = (outPinD/2), fill = False, color = 'red')
+        ax1.add_patch(pin)       
+
+    # Plot both static discs
+    ax1.plot(x_val_offset, y_val_offset, color = 'blue', linewidth = 2)
+    ax1.plot(x_val_180_shifted,y_val_180, color = disc2Color, linewidth = 2)
+
+    #Plot static outlines
     shaft = plt.Circle((0, 0), 0.5, color='black')  # Fixed input shaft center
+    eccenPath = plt.Circle((0,0), e, fill=False, edgecolor='brown',linestyle='--') # Eccentric path outline
+    ringPin_PCD = plt.Circle((0,0),radius = (D/2),fill=False,linestyle='--',edgecolor='red',linewidth=1) #Fixed ring pin PCD outline
+    output_PCD_circ1 = plt.Circle((e,0),radius = (outputPCD/2),fill=False,linestyle='--',edgecolor='DarkRed',linewidth=1) #Disc #1 output ring holes
+    output_PCD_circ2 = plt.Circle((-e,0),radius = (outputPCD/2),fill=False,linestyle='--',edgecolor='DarkRed',linewidth=1) #Disc #2 output ring holes
     ax1.add_patch(shaft)
+    ax1.add_patch(eccenPath)
+    ax1.add_patch(ringPin_PCD)
+    ax1.add_patch(output_PCD_circ1)
+    ax1.add_patch(output_PCD_circ2)
     ax1.set_title('Static Gearbox Diagram')
     plt.grid(True)
     plt.axis('equal')
     plt.show()
-    #eccenPath = plt.Circle((0,0), e, fill=False, edgecolor='brown',linestyle='--')
-
+    
 
 #-----------------------------
 # MAIN CODE
@@ -40,12 +95,15 @@ def plotFullStatic(N, D, d_fp, x_value, y_value, e):
 
 #|||||||||||||||||||||||||||||
 # Define profile parameters
-#|||||||||||||||||||||||||||||
 D = 100 # mm
 N = 30
 n = N - 1
 d_fp = 5 # mm
 e = 0.85 # mm
+output_PCD = 50
+d_outPin = 4
+d_outputPinHole = d_outPin + 2*e 
+N_output = 8
 
 x_val = []
 y_val = []
@@ -53,7 +111,6 @@ phi_val = np.linspace(0, 2*np.pi, 10000, endpoint=False)
 
 #|||||||||||||||||||||||||||||
 # Generate disc profile points
-#|||||||||||||||||||||||||||||
 for phi in phi_val:
     argNum = np.sin(-n*phi)
     argDen = (D/(2*e*N)) -np.cos(-n*phi)
@@ -63,18 +120,8 @@ for phi in phi_val:
     x_val.append(x_current)
     y_val.append(y_current)
 
+#|||||||||||||||||||||||||||||
+#Function Calls
+plotStaticProf(x_val, y_val, D)
+plotFullStatic(N, D, d_fp, x_val,y_val, e, output_PCD, d_outputPinHole, N_output, d_outPin)
 #swEquation(D,d_fp,n,e,N) # function call for equation generator
-
-#|||||||||||||||||||||||||||||
-# Plot static disc profile (at origin)
-#|||||||||||||||||||||||||||||
-fig, ax, = plt.subplots()
-ax.plot(x_val, y_val)
-shaft = plt.Circle((0, 0), 0.5, color='black')  # Fixed input shaft center
-ax.add_patch(shaft)
-ax.set_title('Static Cycloidal Disk Profile')
-plt.grid(True)
-plt.axis('equal')
-plt.show()
-
-plotFullStatic(N, D, d_fp, x_val,y_val, e)
