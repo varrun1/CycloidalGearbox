cce1138f484ba6485f36b7aea299ae7bed51bdaf
Varrun Vijay <varrun82@gmail.com>
2025-07-28 18:56:23 -0400

    added tau calc & documentation

--- FA.py
+++ FA.py
@@ -10,23 +10,36 @@ def force_analysis(
     z_out: int,
     r_out: float,
     pressure_angle_fn, 
-    N_disc
+    N_disc: float
 ):
+    """Performs Force Analysis on Disc(s)'s based on gearbox configuration
+
+    Args:
+        thetas (np.ndarray): range of input rotation angle
+        T_in (float): input torque, max torque of motor
+        e (float): eccentricity
+        r_pin (float): fixed roller pin radius
+        r_pcd (float): PCD radius
+        z_p (int): fixed roller pin count
+        z_out (int): output pin count 
+        r_out (float): output pin radius
+        pressure_angle_fn (string): pressure angle function name
+        N_disc (float): disc number
+    """
 
     Nθ = thetas.size
     # gather all per‐lobe pressure angles alphaₖ(θ)
     alpha = np.vstack([pressure_angle_fn(thetas, e, r_pin, r_pcd, z_p, k+1)
         for k in range(z_p)
-    ])  # shape (z_p, Nθ)
+    ])  # shape (z_p, Nθ) - 2D array of z_p rows and theta (N) columns
 
-   # S1: cam reaction force (constant)
+   # 1: cam reaction force (constant)
     F_cam_total = T_in / e
     F_cam = F_cam_total/N_disc
 
-    # S2: distribute the radial force 
+    # 2: distribute the radial force 
     mask   = (alpha <= (np.pi/2))                 # active‐lobe mask
     counts = mask.sum(axis=0)                # N_active(θ) for each θ_j
-    #print('Active lobes: ',counts) #debugging statement
 
     # radial force per lobe:
     Fr = np.zeros_like(alpha)
@@ -37,33 +50,58 @@ def force_analysis(
     Fn = Fr / np.sin(alpha)                       
     Ft = Fn * np.cos(alpha)
 
-    # S3: output torque from the sum of tangential forces at radius r_pcd
-    T_out = np.sum(Ft * r_pcd, axis=0)    # shape (Nθ,)
+    # 3: output torque from the sum of tangential forces at radius r_pcd
+    T_out = np.sum(Ft * r_pcd, axis=0)    
     F_out = T_out / (z_out * r_out)       # per‐pin radial force
 
     return F_cam, Fn, Ft, Fr, T_out, F_out, alpha
 
+
 def pressure_angle_closed(theta, e, r_pin, r_pcd, zp, k):
-        lam = r_pcd / (e * zp)
-        theta_k = theta + 2 * np.pi * (k - 1) / zp
+    """Pressue angle calculator based on other code
 
-        A = 1 + lam**2 - 2 * lam * np.cos(theta_k)
-        num = -np.sin(theta_k) * A**(-0.5)
+    Args:
+        theta (list): range of input rotation angle
+        e (float): eccentricity
+        r_pin (float): fixed roller pin radius
+        r_pcd (float): PCD radius
+        zp (int): fixed roller pin count
+        k (int): lobe number
 
-        B = (
-            1
-            - 2 * (r_pin / r_pcd) * (lam - np.cos(theta_k)) * A**(-0.5)
-            + (r_pin / r_pcd)**2
-        )
-        den = np.sqrt(B)
+    Returns:
+        float: pressure angle across theta for each pin
+    """
+    lam = r_pcd / (e * zp)
+    theta_k = theta + 2 * np.pi * (k - 1) / zp
 
-        return np.arccos(num / den)
+    A = 1 + lam**2 - 2 * lam * np.cos(theta_k)
+    num = -np.sin(theta_k) * A**(-0.5)
+
+    B = (
+        1
+        - 2 * (r_pin / r_pcd) * (lam - np.cos(theta_k)) * A**(-0.5)
+        + (r_pin / r_pcd)**2
+    )
+    den = np.sqrt(B)
+
+    return np.arccos(num / den)
 
 def shaftForces(T_input, r_shaft, Sy):
+    """Perform force analysis on shaft
+
+    Args:
+        T_input (_type_): input torque from motor
+        r_shaft (_type_): shaft radius
+        Sy (_type_): yield strength of material 
+    """
+    #compute torsional stress 
     conversion = 1e-6
+    print('\n Shaft Force Analysis')
     J = (np.pi/2)*(r_shaft**4) #polar moment of inertia, solid CS
     tau_max = ((T_input*r_shaft)/J)*conversion
-    print('Expected shaft load is (MPa):',np.round(tau_max,4))
+    print('Expected torsional shaft load is (MPa):',np.round(tau_max,4))
+
+    #compute allowable stress using Von mises & Tresca critera
     t_allow_VM = Sy/np.sqrt(3)
     t_allow_Tresca = Sy/2
     SF = 2 # allowing for print defects, etc
@@ -74,7 +112,7 @@ def shaftForces(T_input, r_shaft, Sy):
     print('Tresca Allowable Stress (MPa): ', np.round(t_allow_Tresca,4))
     print('Tresca FOS:', np.round(FOS_tres,4))
     
-    #Backward calculation
+    #Backward calculation for max input motor torque 
     print('Backward Calculation')
     T_in_back = (((t_allow_Tresca/SF)/conversion)*J)/r_shaft #applied SF
     print('Allowable T_in (Nm):', np.round(T_in_back,4))
@@ -92,6 +130,7 @@ def plot_static_disc_forces_numbered(
     prof_y:  np.ndarray,
     scale:   float = 0.002  # tweak so arrows look nice
 ):
+    """ Plots force vectors on active lobes at input rotation angle, theta """
     θ0 = thetas[j]
     z_p = Fr.shape[0]
 
@@ -169,6 +208,17 @@ def plot_static_disc_forces_numbered(
 
 
 def genProfile(e, r_pin, r_pcd, z_p):
+    """Generate cycloidal disc profile (used from other script)
+
+    Args:
+        e (float): eccentricity
+        r_pin (float): fixed roller pin radius
+        r_pcd (float): PCD radius
+        z_p (int): fixed roller pin count
+
+    Returns:
+        list: list of x and y profile points
+    """
     D    = 2*r_pcd
     N    = z_p
     d_fp = r_pin
@@ -191,24 +241,16 @@ def genProfile(e, r_pin, r_pcd, z_p):
     return x, y
 
 def lobe_root_stress_from_profile(
-    Ft:             np.ndarray | float,  # tangential force at the pitch circle
+    Ft:             np.ndarray | float,  # tangential force at the pitch circle of kth lobe
     r_pcd:          float,               # pitch‑circle radius
     x_prof:         np.ndarray,          # raw cycloid outline x coords
     y_prof:         np.ndarray,          # raw cycloid outline y coords
     r_shaft:        float,               # inner bore/shaft radius
     web_thickness:  float,               # b, width of the web section
     allowable:      float                # allowable stress (Pa)
-) -> tuple[np.ndarray, np.ndarray, float, float, np.ndarray]:
+) -> tuple[np.ndarray, np.ndarray, float, float, np.ndarray, np.ndarray,np.ndarray,np.ndarray]:
+    """Computes VM stress on given lobe through root stress method, using bending moment + shear analysis
     """
-    Returns:
-      sigma_b  : bending stress array [Pa]
-      sigma_vm : von Mises stress (abs of sigma_b here) [Pa]
-      c        : distance from neutral axis to outer fiber [m]
-      I        : second moment of area of rectangular web [m^4]
-      FOS      : factor of safety = allowable / sigma_vm
-    """
-
-    # 0) ensure Ft is array
     Ft = np.atleast_1d(Ft).astype(float)
 
     # 1) find root radius & web height
@@ -218,44 +260,54 @@ def lobe_root_stress_from_profile(
     if h <= 0:
         raise ValueError(f"Computed web height h={h:.4g} m is ≤ 0.")
 
-    # 2) section properties
+    # 2) section properties (assume rectangular properties)
     b = web_thickness
-    I = b * h**3 / 12
+    I = (b * h**3) / 12
     c = h / 2
 
-    # 3) lever arm from root to pitch circle
+    # 3) moment arm from root of lobe to pitch circle
     L = r_pcd - r_base
 
-    # 4) prepare output arrays, mask out Ft==0
+    # 4) gen output arrays, mask out Ft==0
     active   = Ft != 0.0
     sigma_b  = np.full_like(Ft, np.nan)
     sigma_vm = np.full_like(Ft, np.nan)
     FOS      = np.full_like(Ft, np.nan)
+    tau_max_array  = np.full_like(Ft, np.nan)
+    sigma_vm_total = np.full_like(Ft, np.nan)
+    FOS_total= np.full_like(Ft, np.nan)
 
+    # 4.5) compute bending moment and shear stresses for active rotation angles of input lobe
     if np.any(active):
         S                = I / c
         M_active         = Ft[active] * L          # bending moment [N·m]
-        print('Bending Moment:', M_active)
         sigma_b_active  = M_active / S              # bending stress [Pa]
-        sigma_vm_active = np.abs(sigma_b_active)    # von Mises for pure bending
-        FOS_active      = allowable / sigma_vm_active
+        sigma_vm_bend = np.abs(sigma_b_active)    # von Mises for pure bending
+        FOS_active      = allowable / (sigma_vm_bend * (1e-6))
 
-        # scatter back
+        tau_max = ((3*Ft[active])/(2*b*h))
+        sigma_vm_full = np.sqrt(sigma_b_active**2 + 3*(tau_max**2))
+        FOS_full = allowable/ (sigma_vm_full * (1e-6))
+
+        # storing current values into respective arrays
         sigma_b [active] = sigma_b_active
-        sigma_vm[active] = sigma_vm_active
+        sigma_vm[active] = sigma_vm_bend
         FOS     [active] = FOS_active
+        tau_max_array[active] = tau_max
+        sigma_vm_total[active] = sigma_vm_full
+        FOS_total[active] = FOS_full
 
-    # debug print (now shows array with nan for inactive)
-    #print('Bending Stress (Pa):', sigma_b)
-
-
-    return sigma_b, sigma_vm, c, I, FOS
+    print('Min Bending FOS:',np.round(np.nanmin(FOS),2))
+    print('Min Total FOS:',np.round(np.nanmin(FOS_total),2))
+        
+    return sigma_b, sigma_vm, c, I, FOS, tau_max_array,sigma_vm_total,FOS_total
      
 
+# MAIN CODE
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
 
-    # parameters
+    # design parameters
     thetas = np.linspace(0, 2*np.pi, 361)
     T_in   =  0.16      # Maximum motor torque
     e      =  0.85/1000     # m
@@ -265,6 +317,7 @@ if __name__ == "__main__":
     z_out  =  8
     r_out  =  55/1000     # m
     r_shaft = 5/1000 #m
+    disc_t = 8/1000 #m
 
     # Material parameters
     Sy_pla = 35 # MPA 
@@ -277,22 +330,26 @@ if __name__ == "__main__":
     else: 
         print('Running dual disc configuration')
 
+    #Force Analysis Function calls
     F_cam, Fn, Ft, Fr, T_out, F_out, alpha_total = force_analysis(thetas, T_in, e, r_pin, r_pcd, z_p, z_out, r_out, pressure_angle_closed, disc_Config)
     shaftForces(T_in, r_shaft, Sy_pla)
     profile_x, profile_y = genProfile(e, r_pin,r_pcd,z_p)
 
-
+    #Plot lobe-specific force analysis over input shaft angle
+    lobeNum = 15 #based on 0-indexing
     plt.figure(figsize=(8,4))
-    plt.plot(np.degrees(thetas), Fr[0], label="Fr (radial)")
-    plt.plot(np.degrees(thetas), Ft[0], label="Ft (tangential)")
-    plt.plot(np.degrees(thetas), Fn[0], label="Fn (normal)")
-    plt.xlabel("θ (°)")
+    plt.plot(np.degrees(thetas), Fr[lobeNum], label="Fr (radial)")
+    plt.plot(np.degrees(thetas), Ft[lobeNum], label="Ft (tangential)")
+    plt.plot(np.degrees(thetas), Fn[lobeNum], label="Fn (normal)")
+    plt.xlabel("Input Angle, θ (°)")
     plt.ylabel("Force (N)")
-    plt.title("Lobe #1 Force Components")
+    plt.title(f'Lobe {lobeNum+1} Force Components')
     plt.legend()
     plt.grid(True)
     plt.tight_layout()
     plt.show()
+
+    j = 0  # index corresponding to theta (input angle)
 
     """
     # plot cam force & per‐pin output force
@@ -319,10 +376,9 @@ if __name__ == "__main__":
     plt.ylabel("Lobe index k")
     plt.title("Normal Force on Each Lobe Over One Revolution")
     plt.show()
-    """
+    
 
     #Force dist across lobes at Theta 
-    j = 0  # pick an angle index
     θ0 = np.deg2rad(j)
     j0 = np.argmin(np.abs(thetas - θ0))
     plt.figure()
@@ -335,28 +391,40 @@ if __name__ == "__main__":
     plt.legend()
     plt.grid(True)
     plt.show()  
-   #"""
+    """
     
-    plot_static_disc_forces_numbered(
-    thetas, 
-    Fr, Ft, Fn, alpha_total, 
-    r_pcd, j, 
-    np.array(profile_x), np.array(profile_y),
-    )
+    #Function call for force vector plot function
+    plot_static_disc_forces_numbered(thetas, Fr, Ft, Fn, alpha_total, r_pcd, j, np.array(profile_x), np.array(profile_y),)
 
-    lobeNum = 27 #based on 0-indexing
-    Ft_lobe = Ft[lobeNum,:] 
-    # 3) call the stress routine
-    sigma_b, σ_vm, c, I, FOS = lobe_root_stress_from_profile(Ft_lobe,r_pcd,np.array(profile_x), np.array(profile_y),r_shaft,web_thickness=0.008, allowable=Sy_pla)
+    # Lobe root stress analysis
+    Ft_lobe = Ft[lobeNum,:]
+    sigma_b, σ_vm, c, I, FOS, tau_m, sigmavm_total, FOS_t = lobe_root_stress_from_profile(Ft_lobe,r_pcd,np.array(profile_x), np.array(profile_y),r_shaft,web_thickness=disc_t, allowable=Sy_pla)
+    active = ~np.isnan(sigma_b) # mask out the inactive θ’s (where Ft==0 → sigma_b is NaN)
 
-    # mask out the inactive θ’s (where Ft==0 → sigma_b is NaN)
-    active = ~np.isnan(sigma_b)
-
-    plt.figure(figsize=(8,4))
+    plt.figure(figsize=(15,6))
+    plt.subplot(3,1,1)
     plt.plot(np.degrees(thetas[active]), sigma_b[active] * 1e-6, 'o-', markersize=4)
     plt.xlabel('Rotation θ (°)')
-    plt.ylabel('Bending Stress σ_b (MPa)')
+    plt.ylabel('Bending Stress (MPa)')
     plt.title(f'Lobe {lobeNum+1} Bending Stress vs θ')
+    plt.xlim(0,360)
+    plt.grid(True)
+    plt.tight_layout()
+
+    plt.subplot(3,1,2)
+    plt.plot(np.degrees(thetas[active]), tau_m[active] * 1e-6, 'o-', markersize=4)
+    plt.xlabel('Rotation θ (°)')
+    plt.ylabel('Shear Stress (MPa)')
+    plt.title(f'Lobe {lobeNum+1} Shear Stress vs θ')
+    plt.xlim(0,360)
+    plt.grid(True)
+    plt.tight_layout()
+
+    plt.subplot(3,1,3)
+    plt.plot(np.degrees(thetas[active]), sigmavm_total[active] * 1e-6, 'o-', markersize=4)
+    plt.xlabel('Rotation θ (°)')
+    plt.ylabel('VM Stress (MPa)')
+    plt.title(f'Lobe {lobeNum+1} VM vs θ')
     plt.xlim(0,360)
     plt.grid(True)
     plt.tight_layout()
